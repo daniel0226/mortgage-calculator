1c60a1f631a99f23f2c23d49ab0c7fba5acf330f
Yong Kim <daniel1996k@icloud.com>
2019-12-16 23:53:07 -0500

    Ui 12 16 2019 (#3)
    * UI improvements
    
    * Improved font
    
    * Added input/Fixed backspace issue/Added default 0/Added keyboard removal
    
    * Moved body up to prevent keyboard blockage

--- app/components/FormLine.js
+++ app/components/FormLine.js
@@ -1,67 +1,41 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import {StyleSheet, Text, View } from 'react-native';
-import NumberTextInput from './NumberTextInput.js';
+import { StyleSheet, Text, View } from "react-native";
+import NumberTextInput from "./NumberTextInput.js";
+import { inputStyle } from "../Styles/style";
 
-const FormLine = ({label, value, valueUpdateCB}) => {
-    //console.log('VALUE UPDATE CB IN FORM LINE ' + valueUpdateCB);
-    //console.log('VALUE UPDATE CB IN FORM LINE ' + typeof valueUpdateCB);
-    const removeNonNumericValues = (label, value) => {
-        if (!value) {
-            return;
-        }
+const FormLine = ({ label, value, valueUpdateCB }) => {
+  //console.log('VALUE UPDATE CB IN FORM LINE ' + valueUpdateCB);
+  //console.log('VALUE UPDATE CB IN FORM LINE ' + typeof valueUpdateCB);
+  const removeNonNumericValues = (label, value) => {
+    if (!value) {
+      value = 0;
+    } else {
+      const regexNotNumberOrDot = /[^0-9.]/g;
+      const regexLeadingNumber = /^0+/;
 
-        const regexNotNumberOrDot = /[^0-9.]/g;
-        const regexLeadingNumber = /^0+/;
+      value = value.replace(regexNotNumberOrDot, "");
+      value = value.replace(regexLeadingNumber, "");
+    }
+    valueUpdateCB(label, value);
+  };
 
-        value.replace(regexNotNumberOrDot, '');
-        value.replace(regexLeadingNumber, '');
-
-        valueUpdateCB(label, value);
-    };
-
-    const inputStyle = StyleSheet.create({
-        inputContainer: {
-            position:           'relative',
-            display:            'flex',
-            flexDirection:      'row',
-            backgroundColor:    'white',
-            width:              '70%',
-            marginBottom:       10,
-            paddingTop:         6,
-            paddingBottom:      6,
-            paddingLeft:        '3.333%',
-            paddingRight:       '3.333%',
-        },
-
-        textContainer:
-        {
-            paddingTop:         15,
-            paddingBottom:      15,
-            marginLeft:         10,
-            width:              '70%',
-            overflow:           'scroll',
-        },
-    });
-
-    return (
-        <View style={inputStyle.inputContainer}>
-            <Text style={inputStyle.textContainer}>
-                {label}
-            </Text>
-            <NumberTextInput
-                label={label}
-                value={value}
-                valueUpdateCB={removeNonNumericValues}
-            />
-        </View>
-    );
+  return (
+    <View style={inputStyle.inputContainer}>
+      <Text style={inputStyle.textContainer}>{label}</Text>
+      <NumberTextInput
+        label={label}
+        value={value}
+        valueUpdateCB={removeNonNumericValues}
+      />
+    </View>
+  );
 };
 
 FormLine.propTypes = {
-    label: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired,
-    valueUpdateCB: PropTypes.func.isRequired
+  label: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  valueUpdateCB: PropTypes.func.isRequired
 };
 
 export default FormLine;
--- app/components/NumberTextInput.js
+++ app/components/NumberTextInput.js
@@ -1,56 +1,42 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import {StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from "react-native";
+import { numStyle } from "../Styles/style";
 
-const NumberTextInput = ({label, value, valueUpdateCB}) => {
-    //console.log(label + " " + value + " @ " + valueUpdateCB);
-    //Label = mortgage years, value = num, valueUpdateCB = function
-    function removeNonNumericValues(label, value, valueUpdateCB) {
-        if (!value)return;
+const NumberTextInput = ({ label, value, valueUpdateCB }) => {
+  //console.log(label + " " + value + " @ " + valueUpdateCB);
+  //Label = mortgage years, value = num, valueUpdateCB = function
+  function removeNonNumericValues(label, value, valueUpdateCB) {
+    if (!value) {
+      value = 0;
+    } else {
+      const regexNotNumberOrDot = /[^0-9.]/g;
+      const regexLeadingNumber = /^0+/;
 
-        const regexNotNumberOrDot = /[^0-9.]/g;
-        const regexLeadingNumber = /^0+/;
-
-        value.replace(regexNotNumberOrDot, '');
-        value.replace(regexLeadingNumber, '');
-
-        return valueUpdateCB(label, value);
+      value = value.replace(regexNotNumberOrDot, "");
+      value = value.replace(regexLeadingNumber, "");
     }
+    return valueUpdateCB(label, value);
+  }
 
-    const numStyle = StyleSheet.create({
-        numContainer: {
-            backgroundColor:    'white',
-            width:              '26.333%',
-            display:            'flex',
-            position:           'relative',
-            flexDirection:      'row',
-            shadowColor:        'black',
-            shadowOffset:       {width:0, height:0},
-            shadowOpacity:      0.45,
-            shadowRadius:       2,
-        },
-        numFont:{
-            fontSize:           30,
-            paddingLeft:        5,
-            width:              '100%',
+  return (
+    <View style={numStyle.numContainer}>
+      <TextInput
+        style={numStyle.numFont}
+        value={value.toString()}
+        onChangeText={text =>
+          removeNonNumericValues(label, text, valueUpdateCB)
         }
-    });
-
-    return (
-        <View style={numStyle.numContainer}>
-            <TextInput
-                style={numStyle.numFont}
-                value={value.toString()}
-                onChangeText={text => removeNonNumericValues(label, text, valueUpdateCB)}
-                keyboardType={'numeric'}
-            />
-        </View>
-    );
+        keyboardType={"numeric"}
+        returnKeyType={"done"}
+      />
+    </View>
+  );
 };
 
 NumberTextInput.propTypes = {
-    value: PropTypes.number.isRequired,
-    valueUpdateCB: PropTypes.func.isRequired
+  value: PropTypes.number.isRequired,
+  valueUpdateCB: PropTypes.func.isRequired
 };
 
 export default NumberTextInput;
